75fccfed130bf9200f4760095c3906e611385cef
Sayak Mukhopadhyay <sayak@beezlabs.com>
2023-06-16 15:14:58 +0530

    tests: fix import issues

--- __tests__/pattern-matcher.test.ts
+++ __tests__/pattern-matcher.test.ts
@@ -1,15 +1,13 @@
-import core from '@actions/core'
+import * as core from '@actions/core'
 import {IFile} from '../src/github-services'
 import {checkChangedFilesAgainstPattern} from '../src/pattern-matcher'
 import {expect, test, beforeEach, afterEach, describe, jest} from '@jest/globals'
 
 const coreDebugSpy = jest.fn(() => {})
-const coreSetFailedSpy = jest.fn(() => {})
 
 describe('pattern-matcher', () => {
   beforeEach(() => {
     jest.spyOn(core, 'debug').mockImplementation(coreDebugSpy)
-    jest.spyOn(core, 'setFailed').mockImplementation(coreSetFailedSpy)
   })
 
   afterEach(() => {
@@ -20,19 +18,17 @@ describe('pattern-matcher', () => {
     const files: IFile[] = givenFiles()
     const pattern = '.*.js'
 
-    await checkChangedFilesAgainstPattern(files, pattern)
-
-    expect(coreSetFailedSpy).toHaveBeenCalledTimes(1)
-    expect(coreDebugSpy).toHaveBeenCalledTimes(0)
+    expect(() => checkChangedFilesAgainstPattern(files, pattern)).toThrowError(
+      new Error(`There is at least one file matching the pattern ${pattern}`)
+    )
   })
 
   test('if non matching pattern is not rejected', async () => {
     const files: IFile[] = givenFiles()
     const pattern = '.*.ts'
 
-    await checkChangedFilesAgainstPattern(files, pattern)
+    checkChangedFilesAgainstPattern(files, pattern)
 
-    expect(coreSetFailedSpy).toHaveBeenCalledTimes(0)
     expect(coreDebugSpy).toHaveBeenCalledTimes(1)
   })
 
@@ -40,9 +36,8 @@ describe('pattern-matcher', () => {
     const files: IFile[] = []
     const pattern = '.*'
 
-    await checkChangedFilesAgainstPattern(files, pattern)
+    checkChangedFilesAgainstPattern(files, pattern)
 
-    expect(coreSetFailedSpy).toHaveBeenCalledTimes(0)
     expect(coreDebugSpy).toHaveBeenCalledTimes(1)
   })
 })
